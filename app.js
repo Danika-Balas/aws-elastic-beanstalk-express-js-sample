bfc127de33ba500646991e88697e339dd8f55982
Danika Balas <danikabalas@gmail.com>
2021-04-11 18:58:51 -0500

    Fixed string

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello MSDS-498! If you can read this, this continuous delivery pipeline is fully hooked up!));
+app.get('/', (req, res) => res.send('Hello MSDS-498! If you can read this, this continuous delivery pipeline is fully hooked up!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
